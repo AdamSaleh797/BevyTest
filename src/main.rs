79503c0049aa1d073193e666588b2bcbd088405c
Adam Saleh <65844871+AdamSaleh797@users.noreply.github.com>
2025-08-09 19:38:42 -0400

    Make color select buttons (#4)
    * trying to get font
    
    * chry button
    
    * button can react to things

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,13 @@ mod shapes;
 mod ui_components;
 
 use bank::BankPlugin;
-use bevy::{DefaultPlugins, app::App, prelude::PluginGroup};
+use bevy::{
+    DefaultPlugins,
+    app::App,
+    asset::{AssetMetaCheck, AssetMode, AssetPlugin},
+    prelude::PluginGroup,
+    utils::default,
+};
 use bevy_world_space::{WorldSpacePlugins, world_init::WorldInitPlugin};
 use circle::CirclePlugin;
 use framerate_counter::FrameratePlugin;
@@ -25,7 +31,11 @@ use crate::{color_mix_resource::ColorMixPlugin, debug::DebugPlugins, ui_componen
 fn main() {
     println!("Git Test");
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            mode: AssetMode::Unprocessed,
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        }))
         .add_plugins(WorldSpacePlugins.set(WorldInitPlugin {
             screen_width: 1280.,
             screen_height: 720.,
--- src/ui_components/button.rs
+++ src/ui_components/button.rs
@@ -1,13 +1,24 @@
 use bevy::{
-    app::{Plugin, Startup},
-    color::Color,
-    ecs::system::Commands,
+    app::{Plugin, Startup, Update},
+    asset::AssetServer,
+    color::{
+        Color,
+        palettes::css::{BLACK, BLUE, WHITE},
+    },
+    ecs::{
+        query::Changed,
+        system::{Commands, Query, Res},
+    },
     hierarchy::{BuildChildren, ChildBuild},
-    ui::{BackgroundColor, JustifyContent, Node, UiRect, Val},
+    text::{JustifyText, TextColor, TextFont, TextLayout},
+    ui::{
+        BackgroundColor, BorderColor, Interaction, JustifyContent, Node, UiRect, Val,
+        widget::{Button, Text},
+    },
     utils::default,
 };
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let container = Node {
         width: Val::Percent(100.0),
         height: Val::Percent(100.0),
@@ -15,24 +26,75 @@ fn setup(mut commands: Commands) {
         ..default()
     };
 
-    let square = Node {
-        width: Val::Px(200.),
-        height: Val::Px(200.),
-        border: UiRect::all(Val::Px(2.)),
-        ..default()
-    };
-
-    let background_color = BackgroundColor(Color::srgb(0.65, 0.65, 0.65));
+    let button = (
+        Button,
+        Node {
+            width: Val::Px(200.),
+            height: Val::Px(200.),
+            border: UiRect::all(Val::Px(2.)),
+            ..default()
+        },
+        BackgroundColor(Color::BLACK),
+        BorderColor(Color::BLACK),
+    );
 
     commands.spawn(container).with_children(|parent| {
-        parent.spawn((square, background_color));
+        parent.spawn((
+            button,
+            Text::new("press"),
+            TextColor(Color::WHITE),
+            TextLayout::new_with_justify(JustifyText::Center),
+            TextFont {
+                // This font is loaded and will be used instead of the default font.
+                font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                font_size: 67.0,
+                ..default()
+            },
+        ));
     });
+}
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interactions: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            &mut BorderColor,
+            &mut Text,
+        ),
+        Changed<Interaction>,
+    >,
+) {
+    for (interaction, mut color, mut border_color, mut text) in &mut interactions {
+        match *interaction {
+            Interaction::Pressed => {
+                text.0 = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = BLUE.into();
+            }
+            Interaction::Hovered => {
+                text.0 = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = WHITE.into();
+            }
+            Interaction::None => {
+                text.0 = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = BLACK.into();
+            }
+        }
+    }
 }
 
 #[derive(Default)]
 pub struct ButtonPlugin;
 impl Plugin for ButtonPlugin {
     fn build(&self, app: &mut bevy::app::App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup)
+            .add_systems(Update, button_system);
     }
 }
