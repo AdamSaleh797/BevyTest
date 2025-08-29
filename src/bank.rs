07ab16276767818f32738fcf5f8c625e62079c3e
ClaytonKnittel <35512940+ClaytonKnittel@users.noreply.github.com>
2025-08-28 17:50:55 -0700

    Bank display is now bars (#6)

--- src/bank.rs
+++ src/bank.rs
@@ -6,7 +6,7 @@ use bevy::{
         schedule::IntoSystemConfigs,
         system::{Commands, Query, Res},
     },
-    math::primitives::CircularSector,
+    math::{Vec2, primitives::Rectangle},
     render::{mesh::Mesh2d, view::Visibility},
     sprite::{ColorMaterial, MeshMaterial2d},
 };
@@ -25,7 +25,8 @@ use crate::{
 
 struct Bank;
 impl Bank {
-    const SECTOR_RADIUS: u32 = 15;
+    const BAR_UNIT_WIDTH: u32 = 30;
+    const BAR_UNIT_HEIGHT: u32 = 20;
 }
 
 #[derive(Component)]
@@ -47,7 +48,10 @@ fn bank_icon(
     icon_color: PrimaryColor,
     rendering_params: &mut RenderingParams,
 ) -> (Mesh2d, MeshMaterial2d<ColorMaterial>) {
-    let bank_icon_shape = CircularSector::from_degrees(Bank::SECTOR_RADIUS as f32, 180.);
+    let bank_icon_shape = Rectangle::from_size(Vec2::new(
+        Bank::BAR_UNIT_WIDTH as f32,
+        Bank::BAR_UNIT_HEIGHT as f32,
+    ));
     let mesh = rendering_params.meshes.add(bank_icon_shape);
     let color = Palette::from(icon_color).to_bevy_color();
     let material = rendering_params.materials.add(color);
@@ -65,6 +69,12 @@ fn setup(
     win_info: Res<WinInfo>,
 ) {
     const WIDTH: WorldUnit = WorldUnit::new(1.5);
+    let height: WorldUnit = WIDTH * (Bank::BAR_UNIT_HEIGHT as f32 / Bank::BAR_UNIT_WIDTH as f32);
+
+    const VERTICAL_PADDING: WorldUnit = WorldUnit::new(0.3);
+
+    let total_width = WIDTH * color_mix.color_mix().bank().capacity_per_color() as f32;
+
     let bank = color_mix.color_mix().bank();
 
     for (row, color) in PrimaryColor::all_colors().enumerate() {
@@ -75,11 +85,12 @@ fn setup(
             };
             let position = Position::new(
                 WorldVec2::new(
-                    WorldUnit::left(win_info.aspect_ratio()) + (count + 1) as f32 * WIDTH,
-                    WorldUnit::top(win_info.aspect_ratio()) - (row + 1) as f32 * WIDTH,
+                    -total_width / 2. + (count + 1) as f32 * WIDTH,
+                    WorldUnit::top(win_info.aspect_ratio())
+                        - (row + 1) as f32 * (height + VERTICAL_PADDING),
                 ),
                 WIDTH,
-                2 * Bank::SECTOR_RADIUS,
+                Bank::BAR_UNIT_WIDTH,
                 0.,
             );
             let (mesh_component, color_component) = bank_icon(color, &mut rendering_params);
